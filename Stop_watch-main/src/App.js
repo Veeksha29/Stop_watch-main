c7ff9497255c7b7c04f07fca0923f978d07033d2
Veeksha <153996558+Veeksha29@users.noreply.github.com>
2025-07-30 00:09:44 +0530

    Add files via upload

---
+++ Stop_watch-main/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StopWatch from './StopWatch';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <StopWatch />
+    </div>
+  );
+}
+
+export default App;
